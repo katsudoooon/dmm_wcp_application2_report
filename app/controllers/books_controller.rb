7cbcd2344d58afca880c5e9cf433a07bf4fbb5fe
katsudoooon <sub62.mail58address@gmail.com>
2021-11-12 15:45:12 +0000

    [Update]データベース反映されてない？

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,8 +7,9 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
     redirect_to user_path(current_user.id)
 
   end
@@ -25,6 +26,11 @@ class BooksController < ApplicationController
   def edit
     @book = Book.new
 
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body, :user_id)
   end
 
 end
