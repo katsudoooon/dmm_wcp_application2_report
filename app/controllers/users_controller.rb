e53d44873c64d48bbb88224fa5bbd1ecec6a22f7
katsudoooon <sub62.mail58address@gmail.com>
2021-11-22 17:40:39 +0000

    [Update]4-10 book updateまで

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = @user.books.page(params[:page]).reverse_order
+    @book = Book.new
 
   end
 
